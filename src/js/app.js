855b7b2feecf62b8ec2b33870827d2b547e6f765
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-06-07 13:37:36 -0700

    updates app with variable assignment adjustment

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,6 @@ App = {
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
